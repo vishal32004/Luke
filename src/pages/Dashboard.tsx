48ef134437537e8e54f9003bdafa39342bc353e1
Vishal Tiwari <vishaltiwari1532004@gmail.com>
2025-03-07 19:58:31 +0530

    Second Commit

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,22 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+const Dashboard = () => {
+  return (
+    <div>
+      <SidebarProvider
+        style={
+          {
+            "--sidebar-width": "350px",
+          } as React.CSSProperties
+        }
+      >
+        <AppSidebar />
+        <main>
+          <SidebarTrigger />
+        </main>
+      </SidebarProvider>
+    </div>
+  );
+};
+
+export default Dashboard;
